e26a5ce603a83587ea015d359982e29471dfaab9
AliMwas <alimwas61@gmail.com>
2021-08-20 03:01:38 +0300

    local storage

--- js/app.js
+++ js/app.js
@@ -16,9 +16,7 @@ let voteImageArray = [];
 let viewImageArray = [];
 let product = [];
 let nameImage = [];
-
-
-function saveLocalStorage()
+function saveToLocalStorage()
 {
     let data1 = JSON.stringify(product);
     localStorage.setItem('product',data1);
@@ -26,16 +24,14 @@ function saveLocalStorage()
 function readLocalStorage()
 {
     let stringObj1 = localStorage.getItem('product');
-  
     
     let normalObj1 = JSON.parse(stringObj1);
- 
 
     if(normalObj1)
     {  
         product = normalObj1;
     }
-    
+   
 }
 readLocalStorage();
 function Image(productName) {
@@ -48,40 +44,6 @@ function Image(productName) {
 
 }
 
-
-
-function Image(productName) {
-    this.pName = productName.split('.')[0];
-    this.imgPath = `Images/${productName}`;
-    this.Votes = 0;
-    this.Views = 0;
-    product.push(this);
-    nameImage.push(this.pName);
-
-
-for (let i = 0; i < arrayOfImages .length; i++) {
-    new Image(arrayOfImages [i])
-}
-
-
-
-
-function randomImage() {
-    return Math.floor(Math.random() * arrayOfImages .length) 
-}
-
-
-
-let firstIndex;
-let scondIndex;
-let thirdIndex;
-
-function renderImg() {
-    firstIndex = randomImage();
-    scondIndex = randomImage();
-    thirdIndex = randomImage();
-
-
 for (let i = 0; i < arrayOfImages .length; i++) {
     new Image(arrayOfImages [i])
 }
@@ -102,7 +64,6 @@ function renderImg() {
     firstIndex = randomImage();
     scondIndex = randomImage();
     thirdIndex = randomImage();
-
 
     while (firstIndex === scondIndex || scondIndex == thirdIndex || firstIndex === thirdIndex || displayImage.includes(firstIndex) || displayImage.includes(scondIndex) || displayImage.includes(thirdIndex)) {
         firstIndex = randomImage();
@@ -119,6 +80,7 @@ function renderImg() {
     displayImage[0]=firstIndex;
     displayImage[1]=scondIndex;
     displayImage[2]=thirdIndex;
+
 
 }
 renderImg();
@@ -162,9 +124,7 @@ function showResult(event) {
         viewImageArray.push(product[i].Views);
     }
     chartRender()
-
-    saveLocalStorage
-
+    saveToLocalStorage();
     btnEl.removeEventListener('click', showResult)
 }
 function chartRender() {
@@ -198,41 +158,3 @@ function chartRender() {
         }
     });
 }
-
-
-
-
-    btnEl.removeEventListener('click', showResult)
-}
-function chartRender() {
-    let ctx = document.getElementById('theChart').getContext('2d');
-    let theChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: nameImage,
-            datasets: [{
-                label: '# Votes',
-                data: voteImageArray,
-                backgroundColor: [
-                    'rgba(255, 0, 0, 0.3)'
-                ],
-               
-            }, {
-                label: '# views',
-                data: viewImageArray,
-                backgroundColor: [
-                    'rgb(60, 179, 113)'
-                ],
-               
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-
